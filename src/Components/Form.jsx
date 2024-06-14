64ee4b59b127d59c97cc5b0412ed0afb278dcd59
gerald ssebaggala <ssgerald710@gmail.com>
2024-06-15 02:02:20 +0300

    update task display

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,17 +1,53 @@
+import { useState } from "react";
+
 const Form = () => {
+  const [task, setTask] = useState("");
+
+  const [taskList, setTaskList] = useState([
+    "Calling parents",
+    "Call my Siblings",
+    "Finish up my projects",
+  ]);
+
+  function handleChange(e) {
+    setTask(e.target.value);
+  }
+
+  function handleAddTask() {
+    setTaskList((prestate) => [...prestate]);
+    console.log("added");
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={(e) => e.preventDefault()}>
         <div className="form-row">
           <label htmlFor="item">New Item:</label>
-          <input type="text" id="item" autoComplete="off" />
+          <input
+            type="text"
+            id="item"
+            autoComplete="off"
+            value={task}
+            onChange={handleChange}
+          />
         </div>
-        <button className="btn add-task_btn">Add Task</button>
+        <button className="btn add-task_btn" onClick={handleAddTask}>
+          Add Task
+        </button>
       </form>
 
       <h1 className="title">Todo List</h1>
 
       <ul className="todo-list">
+        {taskList.map((task, id) => (
+          <li className="task" key={id}>
+            <label htmlFor="">
+              <input type="checkbox" id="checkbox" />
+              {task}
+            </label>
+            <button className="btn delete_btn">Delete</button>
+          </li>
+        ))}
         <li className="task">
           <label htmlFor="">
             <input type="checkbox" id="checkbox" />
