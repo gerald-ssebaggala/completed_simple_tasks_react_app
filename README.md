65c01664012d5ce4e88da7d5443b6adc7af8972d
gerald ssebaggala <ssgerald710@gmail.com>
2024-06-15 12:07:01 +0300

    Update task rendering.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # React + Vite \_\_\_ Todo-task application
+
+- keys are always important wen you map thru an array.
+
+# Why does React need keys?
+
+- Keys tell React which array item each component corresponds to, so that it can match them up later. This becomes important if your array items can move (e.g. due to sorting), get inserted, or get deleted. A well-chosen key helps React infer what exactly has happened, and make the correct updates to the DOM tree.
+
+# Rules of keys
+
+- Keys must be unique among siblings. However, it’s okay to use the same keys for JSX nodes in different arrays.
+- Keys must not change or that defeats their purpose! Don’t generate them while rendering.
+
+# Where to get your key
+
+- Different sources of data provide different sources of keys:
+
+- Data from a database: If your data is coming from a database, you can use the database keys/IDs, which are unique by nature.
+  Locally generated data: If your data is generated and persisted locally (e.g. notes in a note-taking app), use an incrementing counter, [crypto.randomUUID()] or a package like uuid when creating items.
+
+- Array methods like [map() and filter()] can't be avoided
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -4,23 +4,35 @@ const Form = () => {
   const [task, setTask] = useState("");
 
   const [taskList, setTaskList] = useState([
-    "Calling parents",
-    "Call my Siblings",
-    "Finish up my projects",
+    // "Calling parents",
+    // "Call my Siblings",
+    // "Finish up my projects",
   ]);
 
   function handleChange(e) {
     setTask(e.target.value);
   }
 
-  function handleAddTask() {
-    setTaskList((prestate) => [...prestate]);
-    console.log("added");
+  function handleAddTask(e) {
+    e.preventDefault();
+
+    const newTask = {
+      // id: +new Date(),
+      id: crypto.randomUUID(),
+      title: task,
+      completed: false,
+    };
+
+    setTaskList((pretasks) => {
+      return [...pretasks, newTask];
+    });
+    setTask("");
+    console.log(taskList);
   }
 
   return (
     <div>
-      <form onSubmit={(e) => e.preventDefault()}>
+      <form>
         <div className="form-row">
           <label htmlFor="item">New Item:</label>
           <input
@@ -31,7 +43,11 @@ const Form = () => {
             onChange={handleChange}
           />
         </div>
-        <button className="btn add-task_btn" onClick={handleAddTask}>
+        <button
+          type="submit"
+          className="btn add-task_btn"
+          onClick={handleAddTask}
+        >
           Add Task
         </button>
       </form>
@@ -39,19 +55,22 @@ const Form = () => {
       <h1 className="title">Todo List</h1>
 
       <ul className="todo-list">
-        {taskList.map((task, id) => (
-          <li className="task" key={id}>
+        {taskList.map((todoTask) => (
+          <li className="task" key={todoTask.id}>
             <label htmlFor="">
-              <input type="checkbox" id="checkbox" />
-              {task}
+              <input
+                type="checkbox"
+                id="checkbox"
+                checked={todoTask.completed}
+              />
+              {todoTask.title}
             </label>
             <button className="btn delete_btn">Delete</button>
           </li>
         ))}
         <li className="task">
           <label htmlFor="">
-            <input type="checkbox" id="checkbox" />
-            item 1
+            <input type="checkbox" id="checkbox" />s item 1
           </label>
           <button className="btn delete_btn">Delete</button>
         </li>
